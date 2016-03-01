2353f999abf9c67aee4e312ba7aff3b663b71038
Дмитрий <malllkin007@gmail.com>
2016-03-01 17:32:08 +0300

    Завершил лабораторную номер 1

--- de.vogella.osgi.firstbundle/src/de/vogella/osgi/firstbundle/internal/MyThread.java
+++ de.vogella.osgi.firstbundle/src/de/vogella/osgi/firstbundle/internal/MyThread.java
@@ -1,18 +1,35 @@
 package de.vogella.osgi.firstbundle.internal;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.example.e4.rcp.todo.model.*;
+import com.example.e4.rcp.todo.services.*;
+
 public class MyThread extends Thread {
   private volatile boolean active = true;
+  
+  private Consumer<List<Todo>> consumer = MyThread::printTodo;
 
   public void run() {
     while (active) {
       System.out.println("Hello OSGi console");
-      try {
+      try { 
+    	 
+        TodoServiceFactory.getInstance().getTodos(consumer);   
+    	
         Thread.sleep(5000);
       } catch (Exception e) {
         System.out.println("Thread interrupted " + e.getMessage());
       }
     }
   }
+  
+  private static void printTodo(List<Todo> list) {
+      for(Todo todo : list) {
+    	  System.out.println(todo);
+      }
+  } 
 
   public void stopThread() {
     active = false;
